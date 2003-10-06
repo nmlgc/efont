2479c1919045f86015de4dcd9792f75a1d4c6cd4
kanou <kanou>
2003-10-06 13:28:01 +0000

    direct import from Kappa20-0.396 is ready.

--- kochi-substitute/substkit/makefont.sh
+++ kochi-substitute/substkit/makefont.sh
@@ -5,6 +5,9 @@
 #  patch < patches/kg20m.bdf.patch
 #  cp kg20m-u.bdf kg21m-u.bdf
 #  patch < patches/kg21m.bdf.patch
+# fetch http://khdd.net/20dot.fonts/Kappa20-0.396.tar.bz2
+#  tar xvfy Kappa20-0.396.tar.bz2
+#  cd Kappa20-0.396 ; sh makebdf.sh ; cd ..
 # fetch http://www.fdiary.net/~kazuhiko/tmp/wadalab-gothic-descent-1024.sfd.bz2
 #  bzcat wadalab-gothic-descent-1024.sfd.bz2 > wadalab-gothic-descent-1024.sfd
 # fetch http://www.fdiary.net/~kazuhiko/tmp/wadalab-mincho-descent-1024.sfd.bz2
@@ -12,8 +15,10 @@
 # fetch http://www.asahi-net.or.jp/%7Esd5a-ucd/freefonts/Oradano-Mincho/Oradano20030619.tgz
 #  tar xvfz Oradano20030619.tgz oradano.ttf
 pfaedit -script Shrink.pe
+head -30 kappa-20.bdf > kappa-21.bdf
+tail +31 kappa-20.bdf >> kappa-21.bdf
 pfaedit -script ReplaceKanjis.pe  kochi-gothic.ttf Gothic-Fix.sfd G.sfd o.ttf
 rm kochi-gothic-subst-*.bdf gothic-Fix-*.bdf
 pfaedit -script ReplaceKanjis.pe  kochi-mincho.ttf Mincho-Fix.sfd M.sfd o.ttf
-rm G.sfd M.sfd o.ttf kochi-mincho-subst-*.bdf mincho-Fix-*.bdf
+rm G.sfd M.sfd o.ttf kochi-mincho-subst-*.bdf kappa-2?.bdf *-u-20.bdf
 # rm wadalab-*.sfd wadalab-*.sfd~ sasage-mono.ttf k2{0,1}gm-u.bdf *.orig *.rej
